f2a1cc3693b744ebe39a075989f562c33f5abc2b
Jakub Sztandera <kubuxu@protonmail.ch>
2016-10-26 12:14:48 +0200

    Add pskConn

---
+++ psk_conn.go
@@ -0,0 +1,91 @@
+package conn
+
+import (
+	"context"
+	"crypto/cipher"
+	"crypto/rand"
+	"errors"
+
+	salsa20 "github.com/davidlazar/go-crypto/salsa20"
+	mpool "github.com/jbenet/go-msgio/mpool"
+	iconn "github.com/libp2p/go-libp2p-interface-conn"
+)
+
+// we are using buffer pool as user needs their slice back
+// so we can't do XOR cripter in place
+var bufPool = mpool.ByteSlicePool
+
+type pskConn struct {
+	iconn.Conn
+	psk *[32]byte
+
+	writeS20 cipher.Stream
+	readS20  cipher.Stream
+}
+
+func (c *pskConn) Read(out []byte) (int, error) {
+	if c.readS20 == nil {
+		nonce := make([]byte, 24)
+		n, err := c.Conn.Read(nonce)
+		if err != nil {
+			return 0, err
+		}
+		if n != 24 {
+			return 0, errors.New("could not read full nonce")
+		}
+		c.readS20 = salsa20.New(c.psk, nonce)
+	}
+
+	maxn := uint32(len(out))
+	in := bufPool.Get(maxn).([]byte) // get buffer
+	defer bufPool.Put(maxn, in)      // put the buffer back
+
+	in = in[:maxn]
+	n, err := c.Conn.Read(in)
+	if err != nil {
+		return 0, err
+	}
+
+	c.readS20.XORKeyStream(out[:n], in[:n])
+
+	return n, nil
+}
+
+func (c *pskConn) Write(in []byte) (int, error) {
+	if c.writeS20 == nil {
+		nonce := make([]byte, 24)
+		_, err := rand.Read(nonce)
+		if err != nil {
+			return 0, err
+		}
+		_, err = c.Conn.Write(nonce)
+		if err != nil {
+			return 0, err
+		}
+
+		c.writeS20 = salsa20.New(c.psk, nonce)
+	}
+	n := uint32(len(in))
+	out := bufPool.Get(n).([]byte) // get buffer
+	defer bufPool.Put(n, out)      // put the buffer back
+
+	out = out[:n]
+	c.writeS20.XORKeyStream(out, in)
+
+	return c.Conn.Write(out)
+}
+
+var _ iconn.Conn = (*pskConn)(nil)
+
+func newPSKConn(ctx context.Context, psk *[32]byte, insecure iconn.Conn) (iconn.Conn, error) {
+	if insecure == nil {
+		return nil, errors.New("insecure is nil")
+	}
+	if psk == nil {
+		return nil, errors.New("pre-shread key is nil")
+	}
+	return &pskConn{
+		Conn: insecure,
+		psk:  psk,
+	}, nil
+}
---
+++ psk_conn_test.go
@@ -0,0 +1,79 @@
+package conn
+
+import (
+	"bytes"
+	"context"
+	"math/rand"
+	"testing"
+
+	iconn "github.com/libp2p/go-libp2p-interface-conn"
+)
+
+var testPSK = [32]byte{} // null bytes are as good test key as any other key
+
+func setupPSKConns(ctx context.Context, t *testing.T) (iconn.Conn, iconn.Conn) {
+
+	conn1, conn2, _, _ := setupSingleConn(t, ctx)
+	psk1, err := newPSKConn(ctx, &testPSK, conn1)
+	if err != nil {
+		t.Fatal(err)
+	}
+	psk2, err := newPSKConn(ctx, &testPSK, conn2)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return psk1, psk2
+}
+
+func TestPSKSimpelMessges(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.TODO())
+	defer cancel()
+
+	psk1, psk2 := setupPSKConns(ctx, t)
+	msg1 := []byte("hello world")
+	out1 := make([]byte, len(msg1))
+
+	_, err := psk1.Write(msg1)
+	if err != nil {
+		t.Fatal(err)
+	}
+	n, err := psk2.Read(out1)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if n != len(out1) {
+		t.Fatalf("expected to read %d bytes, read: %d", len(out1), n)
+	}
+
+	if !bytes.Equal(msg1, out1) {
+		t.Fatalf("input and output are not the same")
+	}
+}
+
+func TestPSKFragmentation(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.TODO())
+	defer cancel()
+
+	psk1, psk2 := setupPSKConns(ctx, t)
+
+	in := make([]byte, 1000)
+	_, err := rand.Read(in)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	out := make([]byte, 100)
+
+	_, err = psk1.Write(in)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for i := 0; i < 10; i++ {
+		_, err = psk2.Read(out)
+		if !bytes.Equal(in[:100], out) {
+			t.Fatalf("input and output are not the same")
+		}
+		in = in[100:]
+	}
+}
