db8dc1d3d0432ef440fe3f05bf83877e84c07016
Jakub Sztandera <kubuxu@protonmail.ch>
2016-10-28 01:04:16 +0200

    Start implementing codecs

---
+++ codec.go
@@ -0,0 +1,5 @@
+package pnet
+
+var (
+	pathPSKv1 = "/key/swarm/psk/1.0.0/"
+)
--- entry.go
+++ entry.go
@@ -4,8 +4,13 @@ import (
 	"io"
 
 	ipnet "github.com/libp2p/go-libp2p-interface-pnet"
+	mc "github.com/multiformats/go-multicodec"
+	bmux "github.com/multiformats/go-multicodec/base/mux"
 )
 
 func NewProtector(input io.Reader) (ipnet.Protector, error) {
+	input = mc.WrapTransformPathToHeader(input)
+	_ = bmux.AllBasesMux()
+
 	return nil, nil
 }
