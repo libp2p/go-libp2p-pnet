86f670b0eb1d6c307858a863ed482eeea4c23070
Jakub Sztandera <kubuxu@protonmail.ch>
2016-11-24 16:17:19 +0100

    Implement fingerprint

--- protector.go
+++ protector.go
@@ -1,16 +1,31 @@
 package pnet
 
 import (
+	"io"
+
 	iconn "github.com/libp2p/go-libp2p-interface-conn"
 	ipnet "github.com/libp2p/go-libp2p-interface-pnet"
 )
 
-type protector struct {
-	psk *[32]byte
+var _ ipnet.Protector = (*protector)(nil)
+
+func NewProtector(input io.Reader) (ipnet.Protector, error) {
+	psk, err := decodeV1PSKKey(input)
+	if err != nil {
+		return nil, err
+	}
+	f := fingerprint(psk)
+	return &protector{psk, f}, nil
 }
 
-var _ ipnet.Protector = (*protector)(nil)
+type protector struct {
+	psk         *[32]byte
+	fingerprint []byte
+}
 
 func (p protector) Protect(in iconn.Conn) (iconn.Conn, error) {
 	return newPSKConn(p.psk, in)
+}
+func (p protector) Fingerprint() []byte {
+	return p.fingerprint
 }
