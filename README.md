4505c85bcc7f1247b5844df55e99617bd274f5c8
Marten Seemann <martenseemann@gmail.com>
2022-05-25 20:48:48 +0200

    deprecate this repo (#47)
    * deprecate this repo
    
    * release v0.3.0

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# DEPRECATION NOTICE
+
+This package has moved into go-libp2p as a sub-package, github.com/libp2p/go-libp2p/p2p/net/pnet.
+
 go-libp2p-pnet
 ==================
 
--- protector.go
+++ protector.go
@@ -1,18 +1,15 @@
+// Deprecated: This package has moved into go-libp2p as a sub-package: github.com/libp2p/go-libp2p/p2p/net/pnet.
 package pnet
 
 import (
-	"errors"
+	"github.com/libp2p/go-libp2p/p2p/net/pnet"
 	"net"
 
 	ipnet "github.com/libp2p/go-libp2p-core/pnet"
 )
 
 // NewProtectedConn creates a new protected connection
+// Deprecated: use github.com/libp2p/go-libp2p/p2p/net/pnet.NewProtectedConn instead.
 func NewProtectedConn(psk ipnet.PSK, conn net.Conn) (net.Conn, error) {
-	if len(psk) != 32 {
-		return nil, errors.New("expected 32 byte PSK")
-	}
-	var p [32]byte
-	copy(p[:], psk)
-	return newPSKConn(&p, conn)
+	return pnet.NewProtectedConn(psk, conn)
 }
